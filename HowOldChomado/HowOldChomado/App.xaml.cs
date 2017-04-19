6cf8c7f0f143e5e54776deb8bc98e22555e13c41
Madoka Chiyoda <chomado@gmail.com>
2017-04-19 16:18:38 +0900

    プロジェクト ファイルを追加します。

---
+++ HowOldChomado/HowOldChomado/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Prism.Autofac;
+using Prism.Autofac.Forms;
+using HowOldChomado.Views;
+using Xamarin.Forms;
+
+namespace HowOldChomado
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("NavigationPage/MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
