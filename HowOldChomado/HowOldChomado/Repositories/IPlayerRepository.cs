92064837dc6a3b5f3173649e03976223d7c91b8c
Madoka Chiyoda <chomado@gmail.com>
2017-05-02 16:20:26 +0900

    update Xamarin.Forms

--- HowOldChomado/HowOldChomado/Repositories/IPlayerRepository.cs
+++ HowOldChomado/HowOldChomado/Repositories/IPlayerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HowOldChomado.BusinessObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace HowOldChomado.Repositories
 {
-    interface IPlayerRepository
+    public interface IPlayerRepository
     {
+        Task<IEnumerable<Player>> FindAllAsync();
+        Task AddAsync(Player p);
     }
 }
