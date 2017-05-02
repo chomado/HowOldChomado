0031adaaa530901984acf30a59455d4eaca0eeba
Madoka Chiyoda <chomado@gmail.com>
2017-05-02 15:44:37 +0900

    Add Business Objects

---
+++ HowOldChomado/HowOldChomado/BusinessObjects/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowOldChomado.BusinessObjects
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FaceId { get; set; }
+        public byte[] Picture { get; set; }
+        public string DisplayName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ HowOldChomado/HowOldChomado/BusinessObjects/ScoreHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowOldChomado.BusinessObjects
+{
+    class ScoreHistory
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public int Age { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
