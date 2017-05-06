a7cd58fcdf2f66038b4a671be28ffcf2cb81e974
Madoka Chiyoda <chomado@gmail.com>
2017-05-06 09:01:50 -0700

    ignore

--- HowOldChomado/HowOldChomado/Secrets.cs
+++ HowOldChomado/HowOldChomado/Secrets.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HowOldChomado
 {
-    class Secrets
+    public static class Secrets
     {
     }
 }
---
+++ HowOldChomado/HowOldChomado/Secrets_sample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowOldChomado
+{
+    // Secrets.cs ファイルの中身のサンプル。APIキーとか public に公開しちゃまずいやつを格納
+    public static class SecretsSample
+    {
+        public const string CongnitiveServiceFaceApiKey = "test";
+    }
+}
