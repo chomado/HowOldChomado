15b86d01f121dd4276c121bafd81249d9dfe165f
Madoka Chiyoda <chomado@gmail.com>
2017-05-08 12:23:48 -0700

    add primarykey attributes

--- HowOldChomado/HowOldChomado/BusinessObjects/Player.cs
+++ HowOldChomado/HowOldChomado/BusinessObjects/Player.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace HowOldChomado.BusinessObjects
 {
     // 人のデータ
     public class Player
     {
+        [PrimaryKey]
+        [AutoIncrement]
         public int Id { get; set; }
         public string FaceId { get; set; }
         public byte[] Picture { get; set; }
--- HowOldChomado/HowOldChomado/BusinessObjects/ScoreHistory.cs
+++ HowOldChomado/HowOldChomado/BusinessObjects/ScoreHistory.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace HowOldChomado.BusinessObjects
 {
     // FaceAPI で取った記録の履歴
     public class ScoreHistory
     {
+        [PrimaryKey]
+        [AutoIncrement]
         public int Id { get; set; }
         public int PlayerId { get; set; }
         public int Age { get; set; }
